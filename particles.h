6cdc78b7bcc9a4e0ab5081e77c34883b2faf5966
venajustin <venajustin@gmail.com>
2025-04-29 20:39:12 -0700

    spawning particles

---
+++ particles.h
@@ -0,0 +1,26 @@
+#ifndef PARTICLES_H
+#define PARTICLES_H
+
+#include "raymath.h"
+#include "stdlib.h"
+
+Vector3 randomPos(Vector3 min, Vector3 max) {
+        
+    double xrand = rand()/(double)RAND_MAX;
+    double yrand = rand()/(double)RAND_MAX;
+    double zrand = rand()/(double)RAND_MAX;
+    
+    Vector3 out = {
+        Lerp(min.x, max.x, xrand), 
+        Lerp(min.y, max.y, yrand), 
+        Lerp(min.z, max.z, zrand)
+    };
+    return out;
+
+}
+
+
+
+#endif
+
+
